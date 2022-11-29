ee116759ee944c899998b4447a20033a5c4abbaf
Melissa Kipp <melissakipp.az@gmail.com>
2022-11-29 13:32:27 -0700

    Code clean up

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,21 @@
-import Main from './Components/Main/Main'
-import {Provider} from 'react-redux'
-import {BrowserRouter} from 'react-router-dom'
-import {ConfigureStore} from './Redux/configureStore'
-import React, { Component } from 'react';
+import Main from './Components/Main/Main';
+import {Provider} from 'react-redux';
+import {BrowserRouter} from 'react-router-dom';
+import {ConfigureStore} from './Redux/configureStore';
 import './App.css';
 
 const store = ConfigureStore();
 
 function App() {
-  
-
-
   return (
-    <div>
-
-    <Provider store={store}>
-      <BrowserRouter>
-        <Main/>
-      </BrowserRouter>
-    </Provider>
-    </div>
+    <>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Main/>
+        </BrowserRouter>
+      </Provider>
+    </>
   );
-
-
 }
 
 export default App;
