c6e6ed89f515e65d2fd4b71b987d2be7f6626816
Melissa Kipp <melissakipp.az@gmail.com>
2022-12-01 08:26:51 -0700

    Code clean up

--- frontend/src/Components/Home/Home.js
+++ frontend/src/Components/Home/Home.js
@@ -1,4 +1,3 @@
-import {Link} from 'react-router-dom';
 import SearchBar from '../SearchBar/SearchBar';
 import Logo from '../assets/logo--color.svg'
 
@@ -8,11 +7,10 @@ function Home(props) {
 
     return(
         <main className='home-main'>
-            <h1>Welcome!</h1>
             <img className='logo' src={Logo} alt="Logo" />
             <SearchBar placeholder="Enter a zip code" />         
         </main>
-    )
+    );
 }
 
 export default Home;
