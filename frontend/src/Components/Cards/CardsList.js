f6db430877955ad8f1e2942176a43e5296460483
Melissa Kipp <melissakipp.az@gmail.com>
2022-12-01 08:55:59 -0700

    Basic starter code for data cards

---
+++ frontend/src/Components/Cards/CardsList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Card from './Card';
+
+import './CardsList.css';
+
+function CardsList() {
+  return (
+    <section className='cards-list'>
+      <Card />
+    </section>
+  );
+}
+
+export default CardsList
