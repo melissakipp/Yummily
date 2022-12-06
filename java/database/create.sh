78f613378d0387f1fdfddc5186285e085508a791
Melissa Kipp <melissakipp.az@gmail.com>
2022-12-06 14:24:20 -0700

    Modified script to create two other tables

--- java/database/create.sh
+++ java/database/create.sh
@@ -4,4 +4,7 @@ DATABASE=final_capstone
 psql -U postgres -f "$BASEDIR/dropdb.sql" &&
 createdb -U postgres $DATABASE &&
 psql -U postgres -d $DATABASE -f "$BASEDIR/schema.sql" &&
-psql -U postgres -d $DATABASE -f "$BASEDIR/user.sql"
+psql -U postgres -d $DATABASE -f "$BASEDIR/user.sql" &&
+psql -U postgres -d $DATABASE -f "$BASEDIR/restaurants.sql" &&
+psql -U postgres -d $DATABASE -f "$BASEDIR/category.sql" &&
+psql -U postgres -d $DATABASE -f "$BASEDIR/typeOfFood.sql"
